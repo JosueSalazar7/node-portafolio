72e651b35ad577ea1d6a4df3f0fe60fbc7915d23
josue <josue.salazar@epn.edu.ec>
2023-06-26 13:58:08 -0500

    db

--- src/database.js
+++ src/database.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const MONGODB_URI = `mongodb://0.0.0.0:27017/portafolio`
+const MONGODB_URI = 'mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0.gyfwtbm.mongodb.net/${DBNAME}'
 
-connection = async () => {
-  try {
-    await mongoose.connect(MONGODB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-    console.log('Database is connected');
-  } catch (error) {
-    console.log(error);
-  }
-};
+connection = async()=>{
+    try {
+         await mongoose.connect(MONGODB_URI,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true
+        })
+        console.log("Database is connected")
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-module.exports = connection;
+module.exports = connection
