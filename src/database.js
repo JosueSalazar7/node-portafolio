e806d2067c493fa5e1cff93a9f1a38668e201920
josue <josue.salazar@epn.edu.ec>
2023-06-26 10:14:09 -0500

    data base

--- src/database.js
+++ src/database.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb+srv://Josue:<Nandoproyt2023>@cluster0.h2rvxez.mongodb.net/?retryWrites=true&w=majority'
+const MONGODB_URI = process.env.DB_CONNECTION;
 
-connection = async()=>{
-    try {
-         await mongoose.connect(MONGODB_URI,{
-            //advertencia de fnciones
-            useUnifiedTopology:true,
-            useNewUrlParser:true
-        })
-        console.log("Database is connected")
-    } catch (error) {
-        console.log(error);
-    }
-}
+connection = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    console.log('Database is connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = connection
+module.exports = connection;
