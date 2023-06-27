da90ebc1a53d52a9ff39f6b59665c4d31b572655
josue <josue.salazar@epn.edu.ec>
2023-06-26 21:07:32 -0500

    db

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-const MONGODB_URI = 'mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0.gyfwtbm.mongodb.net/${DBNAME}'
+const {DBUSER,DBPASSWORD,DBNAME} = process.env
+const MONGODB_URI = `mongodb+srv://${DBUSER}:${DBPASSWORD}@cluster0.gyfwtbm.mongodb.net/${DBNAME}`
 
 connection = async()=>{
     try {
